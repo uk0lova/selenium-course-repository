c68597d052b4427c169f1873d20ca90fb49c21fc
uk0lova <36922168+uk0lova@users.noreply.github.com>
2019-05-23 17:09:23 +0300

    Задание 3.
    Автоматизирован сценарий для логина в панель администрирования учебного приложения.

--- java-example/src/test/java/ru/stqa/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/MyFirstTest.java
@@ -3,6 +3,7 @@ package ru.stqa;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -38,7 +39,9 @@ public class MyFirstTest {
 
     @Test
     public void myFirstTest() {
-        driver.navigate().to("http://www.carabin.ru");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 }
